16258ce3964da43a946663e9eb959621b9b841c2
SceniQ <tassagaai19@gmail.com>
2025-01-14 22:15:20 +0200

    text block

--- ch1-packages/packageb/B.java
+++ ch1-packages/packageb/B.java
@@ -4,5 +4,9 @@ public class B{
 	public static void main(String... args){
 		A a = new A();
 		System.out.println(a.toString());
+		String textBlock = """
+			"Java Study Guide"
+				by Scott & Me :D!""";
+		System.out.println(textBlock);		
 	}
 }
