1c1ea813358ac61928d481e6a58117addf6a3731
Tebogo Assagaai <tebogo.assagaai@fnb.co.za>
2025-01-15 17:41:53 +0200

    wrapper example

--- ch1-packages/packageb/B.java
+++ ch1-packages/packageb/B.java
@@ -4,5 +4,9 @@ public class B{
 	public static void main(String... args){
 		A a = new A();
 		System.out.println(a.toString());
+		System.out.println(Float.valueOf(1)); //works without casting
+		System.out.println(Byte.valueOf((byte)1)); //doesn't work without casting
+		System.out.println(Short.valueOf((short)1)); //doesn't work without casting
+		int z;
 	}
 }
