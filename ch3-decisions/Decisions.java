02ace576a7de416accd4f469672db1593ddeb802
SceniQ <tassagaai19@gmail.com>
2025-01-30 20:21:32 +0200

    decisions exmaple 1

--- ch3-decisions/Decisions.java
+++ ch3-decisions/Decisions.java
@@ -1,3 +1,25 @@
 public class Decisions{
 	
+	public static void main(String... x){
+		patterMatchingTest(10);
+	}
+	
+	private static void patterMatchingTest(Number number){
+		/**
+		*Java 16 officially introduced pattern matching with if statements and the instanceof operator.
+		Pattern matching is a technique of controlling program flow that only executes a section
+		of code that meets certain criteria.
+		**/
+		
+		//option 1
+		if(number instanceof Integer){
+			Integer data = (Integer)number;
+			System.out.println(data.compareTo(5));
+		}
+		
+		//option 2 - with pattern matching
+		if(number instanceof final Integer data) {
+			System.out.println(data.compareTo(5));
+		}
+	}
 }
